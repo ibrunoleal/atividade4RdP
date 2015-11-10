630cf9bbc5dd41471226e9f598afeb5cf9939518
ibrunoleal <ibrunoleal@gmail.com>
2015-11-09 21:20:41 -0300

    primeira parte do exercicio 1

--- RdPAtividade4/src/exercicio1/Exercicio1.java
+++ RdPAtividade4/src/exercicio1/Exercicio1.java
@@ -1,13 +1,43 @@
 package exercicio1;
 
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 
 public class Exercicio1 {
 
 	public static void main(String[] args) {
 	
 		Exercicio1Functions f = new Exercicio1Functions();
-		System.out.println(f.getMatrizPHI(f.get_Data_I_samples_learning()));
+		RealMatrix PHI = f.getMatrizPHI(f.get_Data_I_samples_learning());
+		
+		RealVector t = f.getData_I_labels_learning();
+		
+		RealVector w = f.wML(PHI, t);
+		
+		RealVector labelsPreditosLearning = new ArrayRealVector(PHI.getRowDimension());
+		for (int i = 0; i < PHI.getRowDimension(); i++) {
+			RealVector xi = PHI.getRowVector(i);
+			double yi = f.yPredito(xi, w);
+			int classificacao = f.classificarY(yi);
+			labelsPreditosLearning.setEntry(i, classificacao);
+		}
 
+		RealMatrix PHI_testing = f.getMatrizPHI(f.getData_I_samples_testing());
+		RealVector t_testing = f.getData_I_labels_testing();
+		
+		RealVector preditosTesting = new ArrayRealVector(PHI_testing.getRowDimension()); 
+		RealVector labelsPreditosTesting = new ArrayRealVector(PHI_testing.getRowDimension());
+		for (int i = 0; i < PHI_testing.getRowDimension(); i++) {
+			RealVector xi = PHI_testing.getRowVector(i);
+			double yi = f.yPredito(xi, w);
+			preditosTesting.setEntry(i, yi);
+			int classificacao = f.classificarY(yi);
+			labelsPreditosTesting.setEntry(i, classificacao);
+		}
+		
+		
 	}
 
 }
--- RdPAtividade4/src/exercicio1/Exercicio1Functions.java
+++ RdPAtividade4/src/exercicio1/Exercicio1Functions.java
@@ -1,37 +1,112 @@
 package exercicio1;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 import br.ufc.arida.bcl.rp20152.atv4.file.FileHandler;
-import weka.core.matrix.Matrix;
+
 
 public class Exercicio1Functions {
 	
 	private final String DATA_I_SAMPLES_LEARNING = "data/data-I-samples-learning.csv";
+	private final String DATA_I_LABELS_LEARNING = "data/data-I-labels-learning.csv";
+	
+	private final String DATA_I_SAMPLES_TESTING = "data/data-I-samples-testing.csv";
+	private final String DATA_I_LABELS_TESTING = "data/data-I-labels-testing.csv";
 	
 	private FileHandler fileHandler;
 	
-	private Matrix data_I_samples_learning;
+	private RealMatrix data_I_samples_learning;
+	
+	private RealVector data_I_labels_learning;
+	
+	private RealMatrix data_I_samples_testing;
+	
+	private RealVector data_I_labels_testing;
 	
 	public Exercicio1Functions() {
 		fileHandler = new FileHandler(DATA_I_SAMPLES_LEARNING, ";");
-		data_I_samples_learning = new Matrix(fileHandler.getMatriz());
+		data_I_samples_learning = new Array2DRowRealMatrix(fileHandler.getMatriz());
+		fileHandler = new FileHandler(DATA_I_LABELS_LEARNING, ";");
+		data_I_labels_learning = new ArrayRealVector(fileHandler.getVetor(0));
+		
+		fileHandler = new FileHandler(DATA_I_SAMPLES_TESTING, ";");
+		data_I_samples_testing = new Array2DRowRealMatrix(fileHandler.getMatriz());
+		fileHandler = new FileHandler(DATA_I_LABELS_TESTING, ";");
+		data_I_labels_testing = new ArrayRealVector(fileHandler.getVetor(0));
 	}
 
-	public Matrix get_Data_I_samples_learning() {
+	public RealMatrix get_Data_I_samples_learning() {
 		return data_I_samples_learning;
 	}
 	
-	public Matrix getMatrizPHI(Matrix m) {
-		Matrix temp = new Matrix(m.getRowDimension(), m.getColumnDimension() + 1);
+	public RealVector getData_I_labels_learning() {
+		return data_I_labels_learning;
+	}
+
+	public RealMatrix getData_I_samples_testing() {
+		return data_I_samples_testing;
+	}
+
+	public RealVector getData_I_labels_testing() {
+		return data_I_labels_testing;
+	}
+
+	public RealMatrix getMatrizPHI(RealMatrix m) {
+		RealMatrix temp = new Array2DRowRealMatrix(m.getRowDimension(), m.getColumnDimension() + 1);
 		for (int i = 0; i < m.getRowDimension(); i++) {
 			for (int j = 0; j < m.getColumnDimension() + 1; j++) {
 				if (j == 0) {
-					temp.set(i, j, 1);
+					temp.setEntry(i, j, 1);
 				} else {
-					temp.set(i, j, m.get(i, j-1));
+					temp.setEntry(i, j, m.getEntry(i, j-1));
 				}
 			}
 		}
 		return temp;
+	}
+	
+	public RealVector wML(RealMatrix PHI, RealVector t) {
+		RealMatrix PHI_t = PHI.transpose();
+		RealMatrix A = PHI_t.multiply(PHI);
+		RealMatrix A_I = MatrixUtils.inverse(A);
+		RealMatrix B = A_I.multiply(PHI_t);
+		RealVector r = B.operate(t);
+		return r;
+	}
+	
+	public double yPredito(RealVector x, RealVector w) {
+		RealMatrix wm = new Array2DRowRealMatrix(w.toArray());
+		RealMatrix wt = wm.transpose();
+		return wt.operate(x).getEntry(0);
+	}
+	
+	public int classificarY(double y) {
+		if (y >= 0.5) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public double taxaDeSemelhanca(RealVector v1, RealVector v2) {
+		int cont = 0;
+		if (v1.getDimension() == v2.getDimension()) {
+			for (int i = 0; i < v1.getDimension(); i++) {
+				if (v1.getEntry(i) == v2.getEntry(i)) {
+					cont++;
+				}
+			}
+			System.out.println("cont: " + cont);
+			System.out.println("N: " + v1.getDimension());
+			double taxa = (cont * 100.0) / v1.getDimension();
+			return taxa;
+		}
+		
+		return 0;
 	}
 	
 }
