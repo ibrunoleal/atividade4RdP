69b353888541780a2087fbb19abc11e256c4a6ab
brunoleal <ibrunoleal@gmail.com>
2015-11-18 19:36:29 -0300

    Exercicio 4 concluido

--- RdPAtividade4/src/br/ufc/arida/bcl/rp20152/atv4/exercicios/ex4/Exercicio4.java
+++ RdPAtividade4/src/br/ufc/arida/bcl/rp20152/atv4/exercicios/ex4/Exercicio4.java
@@ -1,7 +1,12 @@
 package br.ufc.arida.bcl.rp20152.atv4.exercicios.ex4;
 
-import org.apache.commons.math3.linear.ArrayRealVector;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+
+import br.ufc.arida.bcl.rp20152.atv4.entidades.Matriz;
 
 public class Exercicio4 {
 
@@ -9,22 +14,57 @@ public class Exercicio4 {
 		
 		Exercicio4Functions f = new Exercicio4Functions();
 		
-		RealVector yPreditos = new ArrayRealVector(f.getData_III_samples_testing().getRowDimension());
-		for (int i = 0; i < yPreditos.getDimension(); i++) {
-			int classificação = -1;
-			double p = -1;
-			RealVector xi = f.getData_III_samples_testing().getRowVector(i);
-			for (int k = 0; k < 3; k++) {
-				double pi = f.pCkX(xi, k);
-				if (pi > p) {
-					classificação = k;
-					p = pi;
-				}
-			}
-			yPreditos.setEntry(i, classificação);
-		}
-		System.out.println(yPreditos);
+		RealMatrix M0 = f.elementosDaClasseK(0, f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		RealMatrix M1 = f.elementosDaClasseK(1, f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		RealMatrix M2 = f.elementosDaClasseK(2, f.getData_III_samples_learning(), f.getData_III_labels_learning());
 		
+		double pi0 = f.piK(0, f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		double pi1 = f.piK(1, f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		double pi2 = f.piK(2, f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		System.out.println("pi0: " + pi0);
+		System.out.println("pi1: " + pi1);
+		System.out.println("pi2: " + pi2);
+		System.out.println();
+		
+		RealVector u0 = f.mean(M0);
+		RealVector u1 = f.mean(M1);
+		RealVector u2 = f.mean(M2);
+		System.out.println("u0: " + u0);
+		System.out.println("u1: " + u1);
+		System.out.println("u2: " + u2);
+		System.out.println();
+		
+		RealMatrix Sigma = f.sigma(f.getData_III_samples_learning(), f.getData_III_labels_learning());
+		System.out.println("Matriz Sigma:\n" + new Matriz(Sigma));
+		
+		RealVector w0 = f.w(M0, Sigma);
+		RealVector w1 = f.w(M1, Sigma);
+		RealVector w2 = f.w(M2, Sigma);
+		List<RealVector> ws = new ArrayList<RealVector>();
+		ws.add(w0);
+		ws.add(w1);
+		ws.add(w2);
+		System.out.println("w0: " + w0);
+		System.out.println("w1: " + w1);
+		System.out.println("w2: " + w2);
+		System.out.println();
+		
+		double w0_0 = f.w0(u0, Sigma, pi0);
+		double w0_1 = f.w0(u1, Sigma, pi1);
+		double w0_2 = f.w0(u2, Sigma, pi2);
+		List<Double> w0s = new ArrayList<Double>();
+		w0s.add(w0_0);
+		w0s.add(w0_1);
+		w0s.add(w0_2);
+		System.out.println("w0_0: " + w0_0);
+		System.out.println("w0_1: " + w0_1);
+		System.out.println("w0_2: " + w0_2);
+		System.out.println();
+
+		RealVector testingPreditos = f.ysPreditos(f.getData_III_samples_testing(), ws, w0s);
+
+		double taxa = f.taxaDeSemelhanca(testingPreditos, f.getData_III_labels_testing());
+		System.out.println("taxa de acerto: " + taxa + "%");
 	}
 
 }
--- RdPAtividade4/src/br/ufc/arida/bcl/rp20152/atv4/exercicios/ex4/Exercicio4Functions.java
+++ RdPAtividade4/src/br/ufc/arida/bcl/rp20152/atv4/exercicios/ex4/Exercicio4Functions.java
@@ -1,5 +1,7 @@
 package br.ufc.arida.bcl.rp20152.atv4.exercicios.ex4;
 
+import java.util.List;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.MatrixUtils;
@@ -90,11 +92,10 @@ public class Exercicio4Functions {
 		return m;
 	}
 	
-	public RealVector meanK(int k, RealMatrix samples, RealMatrix labels) {
-		RealMatrix mk = elementosDaClasseK(k, samples, labels);
-		VectorialMean vm = new VectorialMean(mk.getColumnDimension());
-		for (int i = 0; i < mk.getRowDimension(); i++) {
-			RealVector xi = mk.getRowVector(i);
+	public RealVector mean(RealMatrix samples) {
+		VectorialMean vm = new VectorialMean(samples.getColumnDimension());
+		for (int i = 0; i < samples.getRowDimension(); i++) {
+			RealVector xi = samples.getRowVector(i);
 			vm.increment(xi.toArray());
 		}
 		return new ArrayRealVector(vm.getResult());
@@ -106,7 +107,7 @@ public class Exercicio4Functions {
 		return cov.getCovarianceMatrix();
 	}
 	
-	public RealMatrix sigma(RealMatrix samples, RealMatrix labels) {
+	public RealMatrix sigma(RealMatrix samples, RealMatrix labels) {		
 		int nRows = covarianceMatrixOfK(0, samples, labels).getRowDimension();
 		int nCols = covarianceMatrixOfK(0, samples, labels).getColumnDimension();
 		RealMatrix Sigma = new Array2DRowRealMatrix(nRows, nCols);
@@ -119,38 +120,77 @@ public class Exercicio4Functions {
 		return Sigma;
 	}
 	
-	public RealVector w(int k, RealMatrix samples, RealMatrix labels) {
-		RealVector uk = meanK(k, samples, labels);
-		RealMatrix SigmaI = MatrixUtils.inverse(sigma(samples, labels));
+	public RealVector w(RealMatrix samples, RealMatrix sigma) {
+		RealVector uk = mean(samples);
+		RealMatrix SigmaI = MatrixUtils.inverse(sigma);
 		return SigmaI.operate(uk);
 	}
 	
-	public double w0(int k, RealMatrix samples, RealMatrix labels) {
-		double pCk = piK(k, samples, labels);
-		RealVector uk = meanK(k, samples, labels);
-		RealMatrix SigmaI = MatrixUtils.inverse(sigma(samples, labels));
-		double ln_pCk = Math.log(pCk);
+	public double w0(RealVector u, RealMatrix sigma, double pi) {
+		RealMatrix SigmaI = MatrixUtils.inverse(sigma);
+		double ln_pCk = Math.log(pi);
 		
-		RealMatrix ukT = new Array2DRowRealMatrix(uk.toArray()).transpose();
-		return ukT.scalarMultiply(-1.0/2.0).multiply(SigmaI).operate(uk).mapAdd(ln_pCk).getEntry(0);
+		RealMatrix ukT = new Array2DRowRealMatrix(u.toArray()).transpose();
+		return ukT.scalarMultiply(-1.0/2.0).multiply(SigmaI).operate(u).mapAdd(ln_pCk).getEntry(0);
 	}
 	
-	public double a(RealVector x, int k, RealMatrix samples, RealMatrix labels) {
-		RealVector wk = w(k, samples, labels);
-		double w0k = w0(k, samples, labels);
-		RealMatrix wkT = new Array2DRowRealMatrix(wk.toArray()).transpose();
-		return wkT.operate(x).mapAdd(w0k).getEntry(0);
+	public double a(RealVector x, RealVector w, double w0) {
+		RealMatrix wT = new Array2DRowRealMatrix(w.toArray()).transpose();
+		return wT.operate(x).mapAdd(w0).getEntry(0);
 	}
 	
-	public double pCkX(RealVector x, int k) {
-		double ak = Math.exp(a(x, k, data_III_samples_learning, data_III_labels_learning));
+	public double pCkX(RealVector x, int k, List<RealVector> ws, List<Double> w0s) {
+		double ak = Math.exp(a(x, ws.get(k), w0s.get(k)));
 		
 		double sum = 0;
 		for (int ki = 0; ki < 3; ki++) {
-			double ai = Math.exp(a(x, ki, data_III_samples_learning, data_III_labels_learning));
+			double ai = Math.exp(a(x, ws.get(ki), w0s.get(ki)));
 			sum += ai;
 		}
 		
 		return ak / sum;
+	}
+	
+	public int classificarX(RealVector x, List<RealVector> ws, List<Double> w0s) {
+		int classificacao = -1;
+		double p = -1;
+		for (int k = 0; k < 3; k++) {
+			double pi = pCkX(x, k, ws, w0s);
+			if (pi > p) {
+				classificacao = k;
+				p = pi;
+			}
+		}
+		return classificacao;
+	}
+	
+	public RealVector ysPreditos(RealMatrix samples, List<RealVector> ws, List<Double> w0s) {
+		RealVector preditos = new ArrayRealVector(samples.getRowDimension());
+		for (int i = 0; i < samples.getRowDimension(); i++) {
+			RealVector xi = samples.getRowVector(i);
+			int classe = classificarX(xi, ws, w0s);
+			preditos.setEntry(i, classe);
+		}
+		return preditos;
+	}
+	
+	public int classePeloLabel(RealVector label) {
+		for (int i = 0; i < label.getDimension(); i++) {
+			if (label.getEntry(i) == 1) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public double taxaDeSemelhanca(RealVector preditos, RealMatrix labels) {
+		double cont = 0;
+		for (int i = 0; i < preditos.getDimension(); i++) {
+			if (preditos.getEntry(i) == classePeloLabel(labels.getRowVector(i))) {
+				cont++;
+			}
+		}
+		double n = preditos.getDimension();
+		return (cont * 100) / n;
 	}
 }
